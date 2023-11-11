60246e988b0b770a4e963ce681cac294aad5b2bf
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-11-12 00:40:05 +0330

    UI improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,11 +42,9 @@
   };
 </script>
 
-<div class="flex flex-col w-full h-full">
-  <div class="flex-initial divider" />
-  <div class="flex-initial grid">
-    <div class="flex gap-4" />
-
+<div class="flex flex-col w-full">
+  <div class="divider" />
+  <div class="grid gap-6">
     <div class="flex gap-4 items-center">
       <div class="avatar">
         <div class="w-12 rounded-full">
@@ -58,44 +56,61 @@
         </div>
       </div>
       {#if remult.user}
-        <div class="text-primary">
+        <div class="text-base-content/60 grow">
           {remult.user.name}
         </div>
 
-        <span>
-          | Using
-          <a class="link link-secondary" href="https://kit.svelte.dev/"
-            >SvelteKit</a
-          >,
-          <a class="link link-secondary" href="https://remult.dev/">Remult</a>,
-          <a class="link link-secondary" href="https://daisyui.com/">DaisyUI</a>
-          |
-        </span>
-
-        <button class="btn btn-ghost" type="button" on:click={() => logout()}
-          >SignOut</button
-        >
+        <button class="btn btn-ghost" type="button" on:click={() => logout()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
+              clip-rule="evenodd"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          SignOut
+        </button>
         <!-- <input
           class="input input-bordered w-full"
           placeholder="Who?"
           bind:value={whoSend}
         /> -->
       {:else}
+        <div class="grow" />
         <a
           class="btn btn-primary"
           href="/auth/signin"
-          data-sveltekit-preload-data="off">Sign in</a
+          data-sveltekit-preload-data="off"
         >
-
-        <span>
-          | Using
-          <a class="link link-secondary" href="https://kit.svelte.dev/"
-            >SvelteKit</a
-          >,
-          <a class="link link-secondary" href="https://remult.dev/">Remult</a>,
-          <a class="link link-secondary" href="https://daisyui.com/">DaisyUI</a>
-          |
-        </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
+              clip-rule="evenodd"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Sign in
+        </a>
       {/if}
     </div>
 
@@ -105,12 +120,36 @@
         placeholder="Your message?"
         bind:value={msg}
       />
-      <button class="btn btn-primary" disabled={remult.user ? false : true}
-        >Send</button
+      <button
+        class="btn lg:btn-wide btn-primary"
+        disabled={remult.user ? false : true}
       >
+        Send
+        <svg
+          class="w-5 h-5 stroke-current"
+          width="16"
+          height="16"
+          viewBox="0 0 48 48"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          ><path
+            d="M16 18L8 26L16 34"
+            stroke="currentColor"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          /><path
+            d="M40 10V23C40 24.6569 38.6569 26 37 26H8"
+            stroke="currentColor"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          /></svg
+        >
+      </button>
     </form>
   </div>
-  <div class="flex-initial divider" />
+  <div class="divider" />
   <div class="grid card">
     <div class="m-4">
       {#each messages as { msg, who, createdAt }}
